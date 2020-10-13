ffd33586143add5df28c797d5ff7491a72e189f9
Elvis-Pan <topanyan@sina.com>
2020-10-13 08:11:29 +0800

    Added random generation of numbers each turn

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # 2048
+The 2048 game and an AI solver for it.
+
+Author: Elvis Pan
+
+Email: ypan2@andrew.cmu.edu
--- game.py
+++ game.py
@@ -3,65 +3,91 @@
 Author: Elvis Pan
 Email: ypan2@andrew.cmu.edu
 """
+import random
 
 
 def merge(temp):
     res = []
+    score = 0
     for i in range(len(temp)):
         if temp[i] is None:
             pass
-        elif i < len(temp)-1 and temp[i+1] == temp[i]:
+        elif i < len(temp) - 1 and temp[i + 1] == temp[i]:
             res.append(temp[i] * 2)
-            temp[i+1] = None
+            score += temp[i] * 2
+            temp[i + 1] = None
         else:
             res.append(temp[i])
     res.extend([0] * (4 - len(res)))
-    return res
+    return res, score
 
 
 class Game:
     def __init__(self):
-        self.board = [[0] * 4] * 4
+        self.board = [[0 for i in range(4)] for j in range(4)]
         self.score = 0
+        self.status = True
 
-    def check(self):
-        pass
+    def start_game(self):
+        self.add_random()
+        self.add_random()
+
+    def add_random(self, max_num=2):
+        spots = []
+        for i in range(4):
+            for j in range(4):
+                if self.board[i][j] == 0:
+                    spots.append((i, j))
+        if len(spots) == 0:
+            self.game_over()
+            return
+        seed = random.randint(0, len(spots) - 1)
+        coord = spots[seed]
+        self.board[coord[0]][coord[1]] = 2
 
     def next(self, direction):  # d = 'u', 'd', 'l', 'r'
+        # TODO: detect whether the move is valid
+        assert self.status
         for i in range(4):
             temp = []
             if direction == 'u':
                 for j in range(4):
                     if self.board[j][i] != 0:
                         temp.append(self.board[j][i])
-                res = merge(temp)
+                res, score = merge(temp)
+                self.score += score
                 for j in range(4):
                     self.board[j][i] = res[j]
             elif direction == 'd':
                 for j in range(4):
-                    if self.board[3-j][i] != 0:
-                        temp.append(self.board[3-j][i])
-                res = merge(temp)
+                    if self.board[3 - j][i] != 0:
+                        temp.append(self.board[3 - j][i])
+                res, score = merge(temp)
+                self.score += score
                 for j in range(4):
-                    self.board[3-j][i] = res[j]
+                    self.board[3 - j][i] = res[j]
             elif direction == 'l':
                 for j in range(4):
                     if self.board[i][j] != 0:
                         temp.append(self.board[i][j])
-                res = merge(temp)
+                res, score = merge(temp)
+                self.score += score
                 for j in range(4):
                     self.board[i][j] = res[j]
             else:
                 assert direction == 'r'
                 for j in range(4):
-                    if self.board[i][3-j] != 0:
-                        temp.append(self.board[i][3-j])
-                res = merge(temp)
+                    if self.board[i][3 - j] != 0:
+                        temp.append(self.board[i][3 - j])
+                res, score = merge(temp)
+                self.score += score
                 for j in range(4):
-                    self.board[i][3-j] = res[j]
+                    self.board[i][3 - j] = res[j]
+        self.add_random()
 
-    def over(self):
-        pass
+    def game_over(self):
+        # TODO: force the game to stop
+        self.status = False
 
 
 if __name__ == "__main__":
@@ -79,4 +105,3 @@ if __name__ == "__main__":
     G.board = [[2,0,2,0],[0,2,2,2],[4,4,4,4],[0,2,0,0]]
     G.next('r')
     print(G.board)"""
-
